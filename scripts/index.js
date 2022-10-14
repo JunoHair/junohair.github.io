b247eccf746019de7913eedbe3ada2aaaa7513f2
MaReunEolEum <junho8590@gmail.com>
2022-10-14 18:09:03 +0900

    Added menu and content

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,18 @@
 const title = document.getElementById('title');
+const menu = document.getElementsByClassName('menu').item(0);
+
 
 title.addEventListener('click', () => {
     alert('뭘 눌러 임마');
+})
+
+menu.addEventListener('click', () => {
+    const sm = document.getElementsByClassName('side-menu').item(0);
+    if (sm.classList.contains('show')) {
+        sm.classList.remove('show');
+        sm.classList.add('hide');
+    } else {
+        sm.classList.remove('hide');
+        sm.classList.add('show');
+    }
 })
