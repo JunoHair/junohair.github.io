9331f907044a00e2cf391148fc4ebc5cb6519f48
junho <60024810+Juno26@users.noreply.github.com>
2022-10-18 21:46:41 +0900

    Edit menu, Add widget

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 const title = document.getElementById('title');
 const menu = document.getElementsByClassName('menu').item(0);
+const screenWrap = document.querySelector('.wrapper-screen');
 
 const menuItems = document.querySelectorAll('.item');
 const contents = document.querySelectorAll('.box-content > [class^=content]');
@@ -10,16 +11,20 @@ title.addEventListener('click', () => {
     alert('뭘 눌러 임마');
 });
 
-menu.addEventListener('click', () => {
+function showMenu() {
     const sm = document.getElementsByClassName('box-menu').item(0);
     if (sm.classList.contains('show')) {
         sm.classList.remove('show');
         sm.classList.add('hide');
+        screenWrap.style.display = 'none';
     } else {
         sm.classList.remove('hide');
         sm.classList.add('show');
+        screenWrap.style.display = 'block';
     }
-});
+}
+
+menu.addEventListener('click', showMenu);
 
 for (let i = 0; i < menuItems.length; i++) {
     menuItems.item(i).addEventListener('click', () => {
@@ -28,5 +33,6 @@ for (let i = 0; i < menuItems.length; i++) {
         contents.item(i).classList.remove('hide');
         contents.item(i).classList.add('show');
         currentMenu = i;
+        showMenu();
     });
 }
