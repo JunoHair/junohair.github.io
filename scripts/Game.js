3706b074463e8b9aaed53f351669e7bf80459413
junho <60024810+Juno26@users.noreply.github.com>
2022-12-28 00:11:31 +0900

    Set base of the game

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,5 +1,34 @@
-const testBtn = document.getElementById('testBtn');
+const gameTableBox = document.getElementsByClassName('gameTable').item(0);
+const table = document.createElement('table');
+const resetBtn = document.getElementById('reset');
+const backBtn = document.getElementById('back');
+let tr, td;
 
-testBtn.addEventListener('click', () => {
-    alert('게임 만들기 귀찮다!!!');
+function getTableByIndex(x, y) {
+    return table.childNodes.item(y).childNodes.item(x);
+}
+
+for (i = 0; i < 19; i++) {
+    tr = document.createElement('tr');
+    for (j = 0; j < 19; j++) {
+        td = document.createElement('td');
+        td.addEventListener('click', (event) => {
+            event.target.style.backgroundColor = 'black';
+        });
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
+}
+gameTableBox.appendChild(table);
+
+resetBtn.addEventListener('click', () => {
+    for (i = 0; i < 19; i++) {
+        for (j = 0; j < 19; j++) {
+            getTableByIndex(j, i).style.backgroundColor = '';
+        }
+    }
+});
+
+backBtn.addEventListener('click', () => {
+    window.history.back();
 });
