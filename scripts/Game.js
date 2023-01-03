40dcfcc24685905a7a4dda2c17f07754319f8120
junho <60024810+Juno26@users.noreply.github.com>
2023-01-04 00:44:07 +0900

    Make game better

--- scripts/Game.js
+++ scripts/Game.js
@@ -11,47 +11,83 @@ const cautionText = document.getElementById('caution');
 const gameCanvas = document.getElementById('gameCanvas');
 const ctx = gameCanvas.getContext('2d');
 
+/**
+ * 패턴 표현 방법
+ * 0: any
+ * 1: 빈칸(금수X)
+ * 2: 막힌 칸(백, 벽, 금수)
+ * 3: 흑
+ * 9: 검사자리
+ * 
+ * 12: 뚫막, 막뚫, 뚫뚫
+ * -n: n이 아님
+ */
+
 const threePats = [
     //2-0
-    [ 0,  1, 1, 0, 2, 0, -1, -1, -1],
-    [ 0,  1, 0, 1, 2, 0, -1, -1, -1],
-    [-2,  0, 1, 1, 2, 0, -2, -1, -1],
+    [1, 3, 3, 1, 9, 1, 0, 0, 0],
+    [1, 3, 1, 3, 9, 1, 0, 0, 0],
+    [12, 1, 3, 3, 9, 1, 12, 0, 0],
+
     //1-1
-    [-1,  0, 1, 0, 2, 1,  0, -1, -1],
-    [-1, -2, 0, 1, 2, 1,  0, -2, -1]
+    [0,  1, 3, 1, 9, 3,  1,  0, 0],
+    [0,  1, 1, 3, 9, 1,  3,  0, 0],
+    [0, 12, 1, 3, 9, 3,  1, 12, 0],
+
+    //0-2
+    [0, 0, 0, 1, 9, 1, 3, 3, 1],
+    [0, 0, 0, 1, 9, 3, 1, 3, 1],
+    [0, 0, 12, 1, 3, 3, 9, 1, 12]
 ];
 
 const fourPats = [
     //3-0
-    [ 1,  1, 1, 0, 2, -1, -1, -1, -1],
-    [ 1,  1, 0, 1, 2, -1, -1, -1, -1],
-    [ 1,  0, 1, 1, 2, -1, -1, -1, -1],
-    [-2,  1, 1, 1, 2, -2, -1, -1, -1],
+    [3, 3, 3, 1, 9, 0, 0, 0, 0],
+    [3, 3, 1, 3, 9, 0, 0, 0, 0],
+    [3, 1, 3, 3, 9, 0, 0, 0, 0],
+    [12, 3, 3, 3, 9, 12, 0, 0, 0],
+    
     //2-1
-    [-1,  1, 1, 0, 2,  1, -1, -1, -1],
-    [-1,  1, 0, 1, 2,  1, -1, -1, -1],
-    [-1, -2, 1, 1, 2,  1, -2, -1, -1]
+    [0, 3, 3, 1, 9, 3, 0, 0, 0],
+    [0, 3, 1, 3, 9, 3, 0, 0, 0],
+    [0, 12, 3, 3, 9, 3, 12, 0, 0]
 ];
 
 const longPats = [
-    [-1, 1, 1, 1, 2, 1,  1, -1, -1],
-    [ 1, 1, 1, 1, 2, 1, -1, -1, -1]
+    [0, 3, 3, 3, 9, 3, 0, 0, 0],
+    [3, 3, 3, 3, 9, 3, 0, 0, 0]
 ]
 
 let tableSize = 15;
-let tdSize, tableArray, firstTdPos, pos, lastBox = [], turn = 0, ended = false;
+let tdSize, tableArray, lastBox = [], turn = 0, ended = false;
 
 function getBoxByIndex(x, y) {
+    if (tableSize * y + x < 0 || tableSize * y + x >= tableSize ** 2 || isNaN(tableSize * y + x)) return null;
     return tableArray.item(tableSize * y + x);
 }
 
-/**
- * x, y좌표 읽어오기
- * @param {Node} target 
- * @returns x, y
- */
-function getPositionByNode(target) {
-    return {x: window.pageXOffset + target.getBoundingClientRect().x, y: window.pageYOffset + target.getBoundingClientRect().y};
+function getIndexByBox(target) {
+    if (!target) return null;
+    let firstTdPos = tableArray.item(0).getBoundingClientRect();
+    return {
+        x: Math.round((window.pageXOffset + target.getBoundingClientRect().x - firstTdPos.x) / tdSize), 
+        y: Math.round((window.pageYOffset + target.getBoundingClientRect().y - firstTdPos.y) / tdSize) 
+    };
+}
+
+function setTurn(t) {
+    turn = t;
+    turnText.textContent = `${turn % 2 == 0? 'BLACK' : 'WHITE'}의 차례입니다. (총 수: ${turn})`;
+}
+
+function setEnded(res) {
+    if (res) {
+        ended = true;
+        isWinText.textContent = `${res.toUpperCase()} 승리!`;
+    } else {
+        ended = false;
+        isWinText.textContent = '아직 아무도 승리하지 않았어요.';
+    }
 }
 
 function drawTableLine() {
@@ -73,17 +109,7 @@ function initBoard() {
         for (j = 0; j < tableSize; j++) {
             let td = document.createElement('td');
             td.addEventListener('click', (event) => {
-                /* 
-                삽질의 흔적 ㅅㅂ
-                
-                pos = getPositionByNode(event.target);
-                firstTdPos = getPositionByNode(tableArray.item(0));
-                console.log('x: %d, y: %d', pos.x, pos.y);
-                ctx.beginPath();
-                ctx.arc(pos.x - firstTdPos.x + tdSize / 2, pos.y - firstTdPos.y + tdSize / 2, tdSize * (2 / 5), 0, Math.PI * 2);
-                ctx.fillStyle = turn % 2 == 0? 'black' : 'white';
-                ctx.fill();
-                */
+                //console.log(getIndexByBox(event.target));
 
                 if (ended) return;
 
@@ -92,9 +118,8 @@ function initBoard() {
                     return;
                 }
                 if (turn % 2 == 0) {
-                    firstTdPos = getPositionByNode(tableArray.item(0));
-                    let judgement = checkForbidden(Math.round((getPositionByNode(event.target).x - firstTdPos.x)/tdSize), 
-                                                   Math.round((getPositionByNode(event.target).y - firstTdPos.y)/tdSize));
+                    const pos = getIndexByBox(event.target);
+                    const judgement = checkForbidden(pos.x, pos.y);
                     if (judgement[0] >= 2) {
                         alert('흑은 삼삼이 금지되어 있습니다.');
                         return;
@@ -110,15 +135,11 @@ function initBoard() {
                 }
                 event.target.style.borderRadius = '50%';
                 event.target.style.backgroundColor = turn % 2 == 0? 'black' : 'white';
+                event.target.className = turn % 2 == 0? 'black' : 'white';
                 lastBox.push(event.target);
     
-                let con = checkWin();
-                if (con) {
-                    ended = true;
-                    isWinText.textContent = `${con.toUpperCase()} 승리!`;
-                } else isWinText.textContent = '아직 아무도 승리하지 않았어요.';
-                turn++;
-                turnText.textContent = `${turn % 2 == 0? 'BLACK' : 'WHITE'}의 차례입니다. (총 수: ${turn})`;
+                setEnded(checkWin());
+                setTurn(turn + 1);
             });
             tr.appendChild(td);
         }
@@ -126,7 +147,6 @@ function initBoard() {
     }
     gameTableBox.appendChild(table);
     tableArray = document.getElementsByTagName('td');
-    firstTdPos = getPositionByNode(tableArray.item(0));
     initSize();
 }
 
@@ -142,27 +162,69 @@ function initSize() {
 }
 
 function resetBoard() {
-    /*
-    삽질의 흔적 2
-
-    ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
-    ctx.beginPath();
-    drawTableLine();
-    */
-
     for (i = 0; i < tableSize ** 2; i++) {
         tableArray.item(i).style.borderRadius = '0';
         tableArray.item(i).style.backgroundColor = '';
+        tableArray.item(i).className = '';
     }
-    ended = 0;
-    turn = 0;
+    setEnded(false);
+    setTurn(0);
     lastBox = [];
-    isWinText.textContent = '아직 아무도 승리하지 않았어요.';
-    turnText.textContent = `${turn % 2 == 0? 'BLACK' : 'WHITE'}의 차례입니다. (총 수: ${turn})`;
 }
 
-function checkForbidden(x, y) {
+function matchPattern(target, pats, reverse) {
+    //console.log(target);
+    let min2C, reversePat;
+    const matchingCallBack = (ansV, ind) => {
+        if (ansV == 0 || ansV == 9) return true;
+        if (ansV == 1) {
+            if (target[ind]?.className != '') return false;
+            /*const testC = checkForbidden(getIndexByBox(target[ind]).x, getIndexByBox(target[ind]).y);
+            if (testC[0] >= 2 || testC[1] >= 2 || testC[2]) return false;*/
+            return true;
+        }
+        if (ansV == 2) {
+            if (target[ind] == null || target[ind].className == 'white') return true;
+            /*const testC = checkForbidden(getIndexByBox(target[ind])?.x, getIndexByBox(target[ind])?.y);
+            if (testC[0] >= 2 || testC[1] >= 2 || testC[2]) return true;*/
+            return false;
+        }
+        if (ansV == 3) return target[ind]?.className == 'black';
+        if (ansV == 12) {
+            if (target[ind] == null || target[ind].className == 'white') min2C++;
+            /*if (target[ind]?.className == '') {
+                const testC = checkForbidden(getIndexByBox(target[ind])?.x, getIndexByBox(target[ind])?.y);
+                if (testC[0] >= 2 || testC[1] >= 2 || testC[2]) min2C++;
+            }*/
+            if (target[ind]?.className == 'black') return false;
+            if (min2C == 2) return false;
+            return true;
+        }
+        return false;
+    };
+    if (reverse) {
+        const con1 = pats.some((ansA) => {
+            min2C = 0;
+            return ansA.every(matchingCallBack);
+        });
+        const con2 = pats.some((ansA) => {
+            min2C = 0;
+            reversePat = ansA;
+            reversePat.reverse();
+            return reversePat.every(matchingCallBack);
+        });
+        return con1 || con2;
+    } else return pats.some((ansA) => {
+        min2C = 0;
+        return ansA.every(matchingCallBack);
+    });
+}
+
+function checkForbidden(x, y, testCheck) {
+    //console.log('금수호출!!');
     //좌 좌상 상 우상
+    if (getBoxByIndex(x, y) == null || getBoxByIndex(x, y)?.style.backgroundColor != '') return [0, 0, false];
+    getBoxByIndex(x, y).className = 'black';
     let checkLists = [[], [], [], []];
 
     for (j = 4; j > 0; j--) {
@@ -177,7 +239,23 @@ function checkForbidden(x, y) {
         }
         checkLists[0].push(x - j >= 0? getBoxByIndex(x - j, y) : null);
     }
-    for (j = 0; j < 5; j++) {
+    if (testCheck) {
+        const testBlack = {
+            style: {
+                backgroundColor: 'black'
+            }
+        };
+        checkLists[0].push(testBlack);
+        checkLists[1].push(testBlack);
+        checkLists[2].push(testBlack);
+        checkLists[3].push(testBlack);
+    } else {
+        checkLists[0].push(getBoxByIndex(x, y));
+        checkLists[1].push(getBoxByIndex(x, y));
+        checkLists[2].push(getBoxByIndex(x, y));
+        checkLists[3].push(getBoxByIndex(x, y));
+    }
+    for (j = 1; j < 5; j++) {
         if (y + j < tableSize) {
             checkLists[1].push(x + j < tableSize? getBoxByIndex(x + j, y + j) : null);
             checkLists[3].push(x - j >= 0? getBoxByIndex(x - j, y + j) : null);
@@ -191,120 +269,21 @@ function checkForbidden(x, y) {
     }
 
     let tC = 0, fC = 0, l = false;
-    let reversePat = [];
-    for (i = 0; i < 4; i++) {
+    for (K = 0; K < 4; K++) {
         let res = [];
-        res[0] = threePats.filter((ansA) => {
-            let min2C = 0;
-            return ansA.every((ansV, ind) => {
-                if (ansV == -2) {
-                    if (checkLists[i][ind] == null || checkLists[i][ind].style.backgroundColor == 'white') min2C++;
-                    else if (checkLists[i][ind].style.backgroundColor == 'black') return false;
-                    if (min2C == 2) return false;
-                    return true;
-                }
-                if (checkLists[i][ind] == null) {
-                    return ansV == -1;
-                }
-                if (checkLists[i][ind].style.backgroundColor == '' && (ind == 4 || ansV == 0)) return true;
-                if (checkLists[i][ind].style.backgroundColor == 'black' && ansV == 1) return true;
-                if (ansV == -1) return true;
-                return false;
-            })
-        });
-        res[1] = threePats.filter((ansA, ri) => {
-            reversePat = ansA;
-            reversePat.reverse();
-            if (ri == threePats.length - 1) return false;
-            let min2C = 0;
-            return reversePat.every((ansV, ind) => {
-                if (ansV == -2) {
-                    if (checkLists[i][ind] == null || checkLists[i][ind].style.backgroundColor == 'white') min2C++;
-                    else if (checkLists[i][ind].style.backgroundColor == 'black') return false;
-                    if (min2C == 2) return false;
-                    return true;
-                }
-                if (checkLists[i][ind] == null) {
-                    return ansV == -1;
-                }
-                if (checkLists[i][ind].style.backgroundColor == '' && (ind == 4 || ansV == 0)) return true;
-                if (checkLists[i][ind].style.backgroundColor == 'black' && ansV == 1) return true;
-                if (ansV == -1) return true;
-                return false;
-            })
-        });
-        res[2] = fourPats.filter((ansA) => {
-            let min2C = 0;
-            return ansA.every((ansV, ind) => {
-                if (ansV == -2) {
-                    if (checkLists[i][ind] == null || checkLists[i][ind].style.backgroundColor == 'white') min2C++;
-                    else if (checkLists[i][ind].style.backgroundColor == 'black') return false;
-                    if (min2C == 2) return false;
-                    return true;
-                }
-                if (checkLists[i][ind] == null) {
-                    return ansV == -1;
-                }
-                if (checkLists[i][ind].style.backgroundColor == '' && (ind == 4 || ansV == 0)) return true;
-                if (checkLists[i][ind].style.backgroundColor == 'black' && ansV == 1) return true;
-                if (ansV == -1) return true;
-                return false;
-            })
-        });
-        res[3] = fourPats.filter((ansA) => {
-            reversePat = ansA;
-            reversePat.reverse();
-            let min2C = 0;
-            return reversePat.every((ansV, ind) => {
-                if (ansV == -2) {
-                    if (checkLists[i][ind] == null || checkLists[i][ind].style.backgroundColor == 'white') min2C++;
-                    else if (checkLists[i][ind].style.backgroundColor == 'black') return false;
-                    if (min2C == 2) return false;
-                    return true;
-                }
-                if (checkLists[i][ind] == null) {
-                    return ansV == -1;
-                }
-                if (checkLists[i][ind].style.backgroundColor == '' && (ind == 4 || ansV == 0)) return true;
-                if (checkLists[i][ind].style.backgroundColor == 'black' && ansV == 1) return true;
-                if (ansV == -1) return true;
-                return false;
-            })
-        });
-        res[4] = longPats.some((ansA) => {
-            return ansA.every((ansV, ind) => {
-                if (checkLists[i][ind] == null) {
-                    return ansV == -1;
-                }
-                if (checkLists[i][ind].style.backgroundColor == '' && (ind == 4 || ansV == 0)) return true;
-                if (checkLists[i][ind].style.backgroundColor == 'black' && ansV == 1) return true;
-                if (ansV == -1) return true;
-                return false;
-            })
-        });
-        res[5] = longPats.some((ansA) => {
-            reversePat = ansA;
-            reversePat.reverse();
-            return reversePat.every((ansV, ind) => {
-                if (checkLists[i][ind] == null) {
-                    return ansV == -1;
-                }
-                if (checkLists[i][ind].style.backgroundColor == '' && (ind == 4 || ansV == 0)) return true;
-                if (checkLists[i][ind].style.backgroundColor == 'black' && ansV == 1) return true;
-                if (ansV == -1) return true;
-                return false;
-            })
-        });
+        res[0] = matchPattern(checkLists[K], threePats, false);
+        res[1] = matchPattern(checkLists[K], fourPats, true);
+        res[2] = matchPattern(checkLists[K], longPats, true);
 
-        //console.log(i);
+        //console.log(K);
         //console.log(res);
 
-        tC += res[0].length;
-        tC += res[1].length;
-        fC += res[2].length;
-        fC += res[3].length;
-        l = l || res[4] || res[5];
+        tC += res[0];
+        fC += res[1];
+        l = l || res[2];
     }
+
+    getBoxByIndex(x, y).className = '';
 
     //console.log(checkLists);
     return [tC, fC, l];
@@ -340,8 +319,10 @@ function isAllStoneSame(stoneArray) {
     (stoneArray.every((v) => v.style.backgroundColor == 'white') || stoneArray.every((v) => v.style.backgroundColor == 'black'));
 }
 
-initBoard();
-initSize();
+window.addEventListener('load', () => {
+    initBoard();
+    initSize();
+});
 
 window.addEventListener('resize', initSize);
 
@@ -349,9 +330,10 @@ undoBtn.addEventListener('click', () => {
     if (lastBox.length == 0) return;
     lastBox[lastBox.length - 1].style.borderRadius = '0';
     lastBox[lastBox.length - 1].style.backgroundColor = '';
+    lastBox[lastBox.length - 1].className = '';
     lastBox.pop();
-    turn--;
-    turnText.textContent = `${turn % 2 == 0? 'BLACK' : 'WHITE'}의 차례입니다. (총 수: ${turn})`;
+    setTurn(turn - 1);
+    setEnded(checkWin());
 });
 
 resetBtn.addEventListener('click', resetBoard);
