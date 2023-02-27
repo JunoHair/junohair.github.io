e55ae62c9d1744b9e0c3f52877b54b02ef6dc804
junho <60024810+Juno26@users.noreply.github.com>
2023-02-27 18:52:10 +0900

    Add custom weather

--- scripts/Widget.js
+++ scripts/Widget.js
@@ -3,8 +3,21 @@ const rainW = document.querySelectorAll('[id^=icon_r]');
 const thunderW = document.querySelectorAll('[id^=icon_t]');
 const arrWeather = [generalW, rainW, thunderW];
 
+const temp = document.querySelector('#text_info > h1');
+const locate = document.getElementById('info_locate');
+
 const whatW = Math.floor(Math.random() * 3);
 arrWeather[whatW].forEach((v) => {
     v.classList.remove('hide');
     v.classList.add('show');
-})
+});
+
+locate.addEventListener('click', () => {
+    locate.textContent = prompt('장소를 입력해주세요.');
+    if (locate.textContent == '') locate.textContent = '서울 종로구';
+});
+
+temp.addEventListener('click', () => {
+    temp.textContent = prompt('기온을 입력해주세요.', '℃');
+    if (temp.textContent == '') temp.textContent = '-273℃';
+});
